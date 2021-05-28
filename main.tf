6a347c52838b8f98159d798a7d56d04d6b07b67d
Jānis Orlovs <janis.orlovs@gmail.com>
2021-05-29 00:45:24 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,1 @@
-provider "helm" {
-  version = "~> 1.0"
-  kubernetes {
-    config_context = var.k8s-context
-    config_path    = "~/.kube/config"
-  }
-}
 
-provider "kubernetes" {
-  config_context = var.k8s-context
-}
